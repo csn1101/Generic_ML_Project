9a9db2214e2c2c6a21f91c1ddc39618904812dbf
csen1101 <csengupta1101@hotmail.com>
2025-03-02 14:06:03 +0530

    Model Training notebook commit

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 import sys
 import os
+import numpy as np 
+from sklearn.metrics import r2_score,mean_absolute_error,mean_squared_error
 
 def add(a,b):
     c = a+b
@@ -12,3 +14,10 @@ def export_dataframe_to_csv(df, file_name):
     file_path = os.path.join(data_dir, f"{file_name}.csv")
     df.to_csv(file_path, index=False)
     print(f"CSV file saved at: {file_path}")
+
+def evaluate_model(true,predicted):
+    mae = mean_absolute_error(true,predicted)
+    mse = mean_squared_error(true,predicted)
+    r2 = r2_score(true,predicted)
+    rmse = np.sqrt(mean_squared_error(true,predicted))
+    return mae,r2,mse,rmse
